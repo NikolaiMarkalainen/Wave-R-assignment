34d35faf915d96edf30be138a93eccd9034877d1
NikolaiMarkalainen <nikolai.markalainen@gmail.com>
2025-12-04 22:59:53 +0200

    Full host on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,4 +19,6 @@ const PORT = process.env.PORT || 5000;
 
 registerRoutes(app);
 app.use(errorMiddleWare);
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(Number(PORT), '0.0.0.0', () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
